e4969208adca4976984034f60407d32839ad6840
root <polina.malinovskaya@gmail.com>
2021-12-03 12:19:22 -0500

    mysql dump

--- srcs/mysql/start.sh
+++ srcs/mysql/start.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 service mysql start
 sleep 5
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DATABASE"
-mysql -u root -e "CREATE USER IF NOT EXISTS '$USER'@'%' IDENTIFIED BY '$PASSWORD'"
-mysql -u root -e "GRANT ALL PRIVILEGES ON $DATABASE.* TO '$USER'@'%'"
-mysql -u root -e "FLUSH PRIVILEGES;" && \
-mysqladmin -u root password $MYSQL_ROOT_PASSWORD
+#mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+#mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+#mysql -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+#mysql -u root -e "FLUSH PRIVILEGES;" && \
+#mysqladmin -u root password $MYSQL_ROOT_PASSWORD
+mysql < data_dump.sql
 service mysql stop
 exec "$@"
